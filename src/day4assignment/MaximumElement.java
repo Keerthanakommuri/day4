227b300668fab5c85917fbb2076c108ff7514412
keerthanakommuri <kommurikeerthana99@gmail.com>
2021-08-27 11:48:53 +0530

    first commit

---
+++ src/day4assignment/MaximumElement.java
@@ -0,0 +1,26 @@
+package day4assignment;
+import java.util.Scanner;
+
+public class MaximumElement {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int i,j;
+		int row=sc.nextInt();
+		int col=sc.nextInt();
+		int[][] arr=new int[row][col];
+		for(i=0;i<row;i++) {
+			for(j=0;j<col;j++) {
+				arr[i][j]=sc.nextInt();
+			}
+		}
+		int max=0;
+		for(i=0;i<row;i++) {
+			for(j=0;j<col;j++){
+				if(max<arr[i][j]) {
+					max=arr[i][j];
+				}
+			}
+		}
+		System.out.println(+max);
+	}
+}
